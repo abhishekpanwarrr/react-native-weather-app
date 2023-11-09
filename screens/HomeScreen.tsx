e53997d143bb1db5e6e2eed7952b5ecc8d034704
Abhishek Panwar <abhishekpanwar1718@gmail.com>
2023-11-09 23:59:14 +0530

    added naviation and home screen

---
+++ screens/HomeScreen.tsx
@@ -0,0 +1,15 @@
+import { Image, SafeAreaView, StatusBar, StyleSheet, Text, View } from 'react-native'
+import React from 'react'
+
+const HomeScreen = () => {
+  return (
+    <SafeAreaView>
+        {/* <StatusBar /> */}
+        {/* <Image source={""} /> */}
+    </SafeAreaView>
+  )
+}
+
+export default HomeScreen
+
+const styles = StyleSheet.create({})
